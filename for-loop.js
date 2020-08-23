c018f6a3abfe7eda76d2ec9c497969bc0b6e6418
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-08-23 15:12:10 +0000

    Initial commit with all the javascripting work so far from Lighthouse prep work

---
+++ for-loop.js
@@ -0,0 +1,13 @@
+let total = 0;
+let limit = 10;
+for (let i = 0; i< 10; i++) {
+  total += i;
+}
+  console.log (total)
+
+  //  let total = 0
+  //const limit = 10
+//for (let i = 0; i < limit; i++) {
+//total += i
+//}
+//console.log(total)
